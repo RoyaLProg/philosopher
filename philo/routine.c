fcfc401e5d978a60523e3e95238aa4c1eb750eba
RoyaLProg <royalfr@protonmail.com>
2022-07-08 01:43:11 +0200

    removing useless include

--- philo/routine.c
+++ philo/routine.c
@@ -6,13 +6,11 @@
 /*   By: ccambium <ccambium@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/22 22:44:53 by ccambium          #+#    #+#             */
-/*   Updated: 2022/07/04 01:06:55 by ccambium         ###   ########.fr       */
+/*   Updated: 2022/07/07 18:30:39 by ccambium         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philosopher.h"
-#include <stdio.h>
-#include <pthread.h>
 
 void	*routine(void *arg)
 {
@@ -23,6 +21,8 @@ void	*routine(void *arg)
 	x = (t_package *)arg;
 	p = x->philo;
 	philo = x->philosopher;
-	printf("je suis dans thread separer :D !\n");
-	pthread_exit(NULL);
+	ft_free(philo->o_head, x);
+	while (!eat(philo, p))
+		if (check_death(philo, p))
+			die(philo, p);
 }
