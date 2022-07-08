d628ef46c136cb17cfbb1dfde439571481681a42
RoyaLProg <royalfr@protonmail.com>
2022-07-08 23:58:04 +0200

    updating header and structures

--- philo/philosopher.h
+++ philo/philosopher.h
@@ -6,7 +6,7 @@
 /*   By: ccambium <ccambium@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/03 23:49:38 by ccambium          #+#    #+#             */
-/*   Updated: 2022/07/08 00:25:08 by ccambium         ###   ########.fr       */
+/*   Updated: 2022/07/08 20:47:06 by ccambium         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,8 +23,7 @@ typedef struct s_philo
 	size_t			times_eat;
 	size_t			n;
 	pthread_mutex_t	*fork;
-	char			forks[2];
-	size_t			lasteat;
+	suseconds_t		lasteat;
 	struct s_philo	*last;
 	struct s_philo	*next;
 }	t_philo;
@@ -36,11 +35,12 @@ typedef struct s_obj
 }	t_obj;
 
 /*
- * param = [nb_philo, time_die, time_eat, time_sleep(, must_eat)]
+ * param = [nb_philo, time_die, time_eat, time_sleep]
 */
 typedef struct s_philosopher
 {
-	long long int	params[4];
+	suseconds_t		params[3];
+	long long int	times_to_eat;
 	t_obj			*o_head;
 	t_philo			*p_head;
 	size_t			start_time;
@@ -62,12 +62,15 @@ void			ft_free(void *ptr, t_philosopher *philo);
 void			free_all(t_philosopher *philo);
 void			init_philos(t_philosopher *philo);
 void			add_end_obj(t_obj *head, t_obj *obj);
-int				check_death(t_philo	*p, t_philosopher *philo);
+char			check_death(t_philo	*p, t_philosopher *philo);
 void			taking_fork(t_philosopher *philo, t_philo *p);
 void			sleeping(t_philosopher *philo, t_philo *p);
 void			thinking(t_philosopher *philo, t_philo *p);
 void			eating(t_philosopher *philo, t_philo *p);
 void			death(t_philosopher *philo, t_philo *p);
-long long int	will_die(t_philosopher *philo, t_philo *p, long long int t);
+suseconds_t		will_die(t_philosopher *philo, t_philo *p, long long int t);
+size_t			ft_strlen(const char *s);
+void			ft_error(const char *s);
+void			ft_info(const char *s);
 
 #endif
