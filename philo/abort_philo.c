ff0521a0f6c50855a9711822c01a4ce9feae489c
RoyaLProg <royalfr@protonmail.com>
2022-07-08 23:54:49 +0200

    abort_philo.c

---
+++ philo/abort_philo.c
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   abort_philo.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ccambium <ccambium@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/07/08 20:10:46 by ccambium          #+#    #+#             */
+/*   Updated: 2022/07/08 20:18:37 by ccambium         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "philosopher.h"
+
+void	abort_philo(t_philosopher *philo, size_t n)
+{
+	size_t	i;
+	t_philo	*p;
+
+	ft_info("Aborting philosophers...");
+	i = -1;
+	p = philo->p_head;
+	while (++i < n)
+	{
+		pthread_detach(p->thread);
+		if (p->fork != NULL)
+			pthread_mutex_destroy(p->fork);
+		p = p->next;
+	}
+}
