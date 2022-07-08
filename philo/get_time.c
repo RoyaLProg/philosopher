bf9e48fd72a2aeb2d400609d0f5fbf5f7d396b99
RoyaLProg <royalfr@protonmail.com>
2022-07-08 23:55:20 +0200

    get_time.c

---
+++ philo/get_time.c
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_time.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ccambium <ccambium@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/07/08 19:34:11 by ccambium          #+#    #+#             */
+/*   Updated: 2022/07/08 19:47:29 by ccambium         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "philosopher.h"
+
+suseconds_t	get_time(void)
+{
+	struct timeval	tv;
+	suseconds_t		time;
+
+	if (gettimeofday(&tv, NULL) != 0)
+	{
+		ft_error("Could not read time !");
+		return (0);
+	}
+	time = tv.tv_sec * 1000000 + tv.tv_usec;
+	return (time);
+}
